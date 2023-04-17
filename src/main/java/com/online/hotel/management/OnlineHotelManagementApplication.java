dd4453a5087514ae2c91b07a49023aa11cba3282
Divya59-Patil <divyapatil200259@gmail.com>
2023-04-17 13:57:35 +0530

    first commit in online management system

---
+++ src/main/java/com/online/hotel/management/OnlineHotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.online.hotel.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineHotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineHotelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/hotel/management/OnlineHotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.hotel.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineHotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
